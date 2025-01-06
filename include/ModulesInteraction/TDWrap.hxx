dae4bd8cdbaf6eaf044b8d54d365525d9fb515a9
username000101 <cxx.username000101@gmail.com>
2025-01-06 21:46:32 +0300

    Make lit::modules_interaction::TdWrap free(now you can just copy and paste the code from include/ModulesInteraction/TdWrap.hxx into your module)

--- include/ModulesInteraction/TDWrap.hxx
+++ include/ModulesInteraction/TDWrap.hxx
@@ -1,30 +1,46 @@
 #ifndef LIT_MODULESINTERACTION_TDWRAP_HXX
 #define LIT_MODULESINTERACTION_TDWRAP_HXX
 
+#include <cstdint>
+
 #include <td/telegram/Client.h>
 
 namespace lit {
+    namespace runtime_storage {
+        extern std::uint64_t LITRequestId;
+    }
+
     namespace modules_interaction {
         /*
-         * TdWrap is a method of abstracting the Stdlib client from the end module.
+         * TdWrap is a method of abstracting the tdlib client from the module.
          * TdWrap provides wrappers for interacting with the Telegram API without providing direct access to the TDLib instance
          */
-        class TdWrap {
+        class __attribute__((visibility("default"))) TdWrap {
         public:
-            typedef td::ClientManager::Response(*update_handler)
-                    (td::td_api::object_ptr<td::td_api::Function>, td::ClientManager::ClientId);
+            using update_handler = td::ClientManager::Response(*)
+                (td::td_api::object_ptr<td::td_api::Function>, td::ClientManager::RequestId);
 
             TdWrap operator=(TdWrap& other) = delete;
             TdWrap(TdWrap& other) = delete;
             TdWrap(TdWrap&& other) = delete;
-            TdWrap(update_handler update_function) : update_function_(update_function) {}
+            TdWrap(update_handler update_function, const std::string& call_command,
+                   td::td_api::object_ptr<td::td_api::message> message) :
+                update_function_(update_function), call_command_(call_command),
+                message_(std::move(message)) {}
 
             [[nodiscard]] td::ClientManager::Response send_request
                    (td::td_api::object_ptr<td::td_api::Function> request) const noexcept
-                   { return this->update_function_(std::move(request), this->td_request_id_++); }
+                   { return this->update_function_(std::move(request), runtime_storage::LITRequestId++); }
+
+            [[nodiscard]] const td::td_api::object_ptr<td::td_api::message>& get_message() const noexcept
+                   { return this->message_; }
+
+            [[nodiscard]] std::string get_call_command() const noexcept
+                   { return this->call_command_; }
         private:
-            mutable td::ClientManager::RequestId td_request_id_;
             update_handler update_function_;
+            std::string call_command_;
+            td::td_api::object_ptr<td::td_api::message> message_;
         };
     }
 }
