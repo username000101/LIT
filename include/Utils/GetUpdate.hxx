0e9484da76f5ab844c715c64eea7daf859cd10d1
username000101 <cxx.username000101@gmail.com>
2025-02-01 22:48:47 +0300

    Change mutex type for std::lock_guard

--- include/Utils/GetUpdate.hxx
+++ include/Utils/GetUpdate.hxx
@@ -2,17 +2,15 @@
 #define LIT_UTILS_GETUPDATE_HXX
 
 #include <td/telegram/Client.h>
-
 #include <RuntimeStorage/RuntimeStorage.hxx>
 
 namespace utils {
     [[nodiscard]] inline td::ClientManager::Response get_td_updates(double timeout = LIT_TDLIB_TIMEOUT) {
         using lit::runtime_storage::LITClient;
-        using lit::runtime_storage::LITMutex;
+        using lit::runtime_storage::UPDMutex;
 
-        LITMutex.use("get_td_updates");
+        std::lock_guard<yamc::fair::mutex> lock(UPDMutex);
         auto response = LITClient->receive(timeout);
-        LITMutex.free("get_td_updates");
         return response;
     }
 }
