58a8fc9f15631c32dbbad9da9b49e1c1aa28e709
username000101 <cxx.username000101@gmail.com>
2025-02-01 22:49:43 +0300

    Save incoming updates and check for updates locally

--- src/TelegramAPIInteraction/Updates.cxx
+++ src/TelegramAPIInteraction/Updates.cxx
@@ -1,6 +1,7 @@
 #include <TelegramAPIInteraction/Updates.hxx>
 
 #include <algorithm>
+#include <unordered_map>
 
 #include <td/telegram/Client.h>
 #include <Utils/Macros.hxx>
@@ -11,6 +12,8 @@
 #include <Utils/GetUpdate.hxx>
 #include <TelegramAPIInteraction/Loop.hxx>
 
+static std::unordered_map<td::ClientManager::RequestId, td::ClientManager::Response> unconfirmed_updates;
+
 td::ClientManager::Response lit::td_api::get_response(td::td_api::object_ptr<td::td_api::Function> req, td::ClientManager::RequestId req_id) {
     static auto logger = spdlog::get("LIT");
     ASSERT(logger, "The 'LIT' logger is not initialized");
@@ -19,7 +22,8 @@ td::ClientManager::Response lit::td_api::get_response(td::td_api::object_ptr<td:
     using runtime_storage::LITConfig;
     using runtime_storage::LITRequestId;
     using runtime_storage::LITClientId;
-    using runtime_storage::LITMutex;
+    using runtime_storage::REQMutex;
+
 
     if (req_id == 0)
         return {};
@@ -33,6 +37,15 @@ td::ClientManager::Response lit::td_api::get_response(td::td_api::object_ptr<td:
                 "{}: Getting response for request_id {} and td::td_api::Function::ID {}",
                 __PRETTY_FUNCTION__, req_id, req->get_id());
 
+    if (unconfirmed_updates.count(req_id) > 0) {
+        logger->log(spdlog::level::debug,
+                    "{}: The response to the request with id {} was found locally",
+                    __PRETTY_FUNCTION__, req_id);
+        auto response_tmp = std::move(unconfirmed_updates.at(req_id));
+        unconfirmed_updates.erase(req_id);
+        return std::move(response_tmp);
+    }
+
     LITClient->send(LITClientId, req_id, std::move(req));
     for (int attempts = 1; attempts < LIT_TDLIB_ATTEMPTS; ++attempts) {
         auto result = utils::get_td_updates();
@@ -40,10 +53,11 @@ td::ClientManager::Response lit::td_api::get_response(td::td_api::object_ptr<td:
             logger->log(spdlog::level::warn,
                 "{}: Invalid response received(object == nullptr; request_id == {})",
                 __PRETTY_FUNCTION__, result.request_id);
-        } else if (result.request_id != req_id) {
+        } else if (result.request_id != req_id && result.request_id != 0) {
             logger->log(spdlog::level::trace,
                 "{}: Received response with request_id == {} but expected {}",
                 __PRETTY_FUNCTION__, result.request_id, req_id);
+            unconfirmed_updates[result.request_id] = std::move(result);
         } else
             return result;
     }
