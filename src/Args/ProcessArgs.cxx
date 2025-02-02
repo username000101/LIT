dbfb7440df3d0b27231bc9c42b202d2846491599
username000101 <cxx.username000101@gmail.com>
2025-02-02 13:49:01 +0300

    Change the logic of argument processing

--- src/Args/ProcessArgs.cxx
+++ src/Args/ProcessArgs.cxx
@@ -10,8 +10,16 @@ void lit::args::process_args(int argc, char** argv, std::unordered_map<std::stri
     }
 
     for (int i = 1; i < argc && argv[i]; ++i) {
-        if (args.contains(argv[i]))
-            args.at(argv[i])(std::nullopt);
+        if (args.contains(argv[i])) {
+            std::vector<std::string> arguments;
+            for (int j = i + 1; j < argc && argv[j]; ++j)
+                arguments.push_back(argv[j]);
+
+            if (arguments.empty())
+                args.at(argv[i])(std::nullopt);
+            else
+                args.at(argv[i])(arguments);
+        }
         else
             spdlog::log(spdlog::level::warn,
                 "The argument does not match any of the established ones (it will be skipped)");
